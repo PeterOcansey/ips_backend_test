4e2a51e56ddaf7afec4ba2441f91910ab107074c
peter <ocanseypeter@gmail.com>
2022-01-08 06:33:19 +0000

    feat: Handling CommentWrittenNotify event listener, Notify AchievementUnlocked event

--- app/Listeners/CommentWrittenNotifier.php
+++ app/Listeners/CommentWrittenNotifier.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\CommentWritten;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use App\IPS\Utils\AchievementUtil;
 
 class CommentWrittenNotifier
 {
@@ -27,5 +28,12 @@ class CommentWrittenNotifier
     public function handle(CommentWritten $event)
     {
         //
+        $user = $event->comment->user;
+        
+        $userAchievement = AchievementUtil::getCommentsWrittenAchievement(count($user->comments));
+        if($userAchievement != null)
+        {
+            event(new AchievementUnlocked($userAchievement, $user));
+        }
     }
 }
