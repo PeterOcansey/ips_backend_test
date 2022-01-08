4ff488ed863cffb7103f31ae9bc49c6726920bb0
peter <ocanseypeter@gmail.com>
2022-01-08 08:53:14 +0000

    feat: Handling StoreUnlockedAchievementNotifier to add a new achievement

--- app/Listeners/StoreUnlockedAchievementNotifier.php
+++ app/Listeners/StoreUnlockedAchievementNotifier.php
@@ -5,17 +5,20 @@ namespace App\Listeners;
 use App\Events\AchievementUnlocked;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use App\IPS\Activities\UserAchievementActivity;
 
 class StoreUnlockedAchievementNotifier
 {
+    protected $userAchievementActivity;
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(UserAchievementActivity $userAchievementActivity)
     {
         //
+        $this->userAchievementActivity = $userAchievementActivity;
     }
 
     /**
@@ -27,5 +30,7 @@ class StoreUnlockedAchievementNotifier
     public function handle(AchievementUnlocked $event)
     {
         //
+        $this->userAchievementActivity->addAchievement($event->achievement_name, $event->user); 
+        
     }
 }
