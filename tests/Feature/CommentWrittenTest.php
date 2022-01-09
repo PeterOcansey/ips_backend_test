613a0ce02492182b7ac845419a0e42dd78a52b8a
peter <ocanseypeter@gmail.com>
2022-01-09 01:39:03 +0000

    refac: Added comment to test cases

--- tests/Feature/CommentWrittenTest.php
+++ tests/Feature/CommentWrittenTest.php
@@ -13,9 +13,12 @@ use App\Listeners\CommentWrittenNotifier;
 class CommentWrittenTest extends TestCase
 {
     //use RefreshDatabase;
+
     /**
-     * A basic feature test example.
-     *
+     * Test a comment can be created
+     * Test functionality of CommentWrittenNotifier listener when CommentWritten event is triggered
+     * Assert our database has the right record
+     * 
      * @return void
      */
     public function test_comment_written()
@@ -32,6 +35,11 @@ class CommentWrittenTest extends TestCase
         $this->assertModelExists($comment);
     }
 
+    /**
+     * Assert our CommentWrittenNotifier event listener is attached to the CommentWritten event
+     * 
+     * @return void
+     */
     public function test_is_attached_to_event()
     {
         Event::fake();
